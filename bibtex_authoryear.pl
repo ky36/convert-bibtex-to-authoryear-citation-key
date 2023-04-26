5e3654cf656c743b530a7fbafa1e05ca1a78a263
ky36 <7727487+ky36@users.noreply.github.com>
2023-04-25 20:59:24 -0400

    Add files via upload

--- bibtex_authoryear.pl
+++ bibtex_authoryear.pl
@@ -18,20 +18,7 @@ while (my $li = <>) {
     chomp($li);
 
     if ($li =~ /^\s*@/) { ## flush previous record
-	if (@one) {
-	    ($year) = $year =~ /(\d+)/;
-	    ($author) = $author =~ /[{"'\s]*(.*?),/;
-
-	    ### get rid of special char
-	    $author =~ s/[{}]//g;
-	    $author =~ s/\\.//g;
-	    
-	    $key = $author . $year;
-
-	    $key =~ s/\s//g;
-	    
-	    &printout_this($key) if (@one);
-	}
+	&printout_this($key) if (@one);
 	$header = $li;
 	@one = ();
 	next;
@@ -46,23 +33,36 @@ while (my $li = <>) {
 	$year = $1;
     }
 }
+
 &printout_this($key) if (@one);
 
 	  
 sub printout_this {
-    my ($key, ) = @_;
-    #print "$key\n";
-    if (!defined($h{$key})) {
-	$h{$key} = 'a';
-    } else {
-	$h{$key}++;
-	$key .= $h{$key};
-    }
-    #print "$author, $year, $key\n";	
+    if (@one) {
+	($year) = $year =~ /(\d+)/;
+	($author) = $author =~ /[{"'\s]*(.*?),/;
 
-    my $newheader = $header;
-    $newheader =~ s/\{(.*),/{$key,/; 
-    
-    print "$newheader\n";
-    print map("$_\n", @one);
+	### get rid of special char
+	$author =~ s/[{}]//g;
+	$author =~ s/\\.//g;
+	
+	$key = $author . $year;
+
+	$key =~ s/\s//g;
+
+	#print "$key\n";
+	if (!defined($h{$key})) {
+	    $h{$key} = 'a';
+	} else {
+	    $h{$key}++;
+	    $key .= $h{$key};
+	}
+	#print "$author, $year, $key\n";	
+	
+	my $newheader = $header;
+	$newheader =~ s/\{(.*),/{$key,/; 
+	
+	print "$newheader\n";
+	print map("$_\n", @one);
+    }
 }
